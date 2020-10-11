ad3c3ba32e806844ffef69c843bc87186b713366
Dustin Jackson <dustin.guy.jackson@gmail.com>
2020-10-11 13:12:05 -0700

    created edge cases for stack, and starter files for other data structures

--- js/stacks.js
+++ js/stacks.js
@@ -8,18 +8,34 @@ class Stack {
     }
 
     push(data) {
+        if(data === undefined || null) {
+            throw console.error("You did not push a value to the stack.");
+        }
         this._storage[this._length] = data;
         this._length++;
 
     }
 
     pop() {
+        //edge case for popping empty stack
+        if(this._length === 0) {
+            throw console.error("Nothing in the stack.")
+        }
+
         const result = this._storage[this._length - 1];
         this._storage[this._length - 1] = undefined;
         this._length--;
         return result;
     }
 
+    peek() {
+        //edge case for peeking empty stack
+        if(this._length === 0) {
+            throw console.error("Nothing in the stack.");
+        }
+
+        return this._storage[this._length - 1];
+    }
 };
 
 const stack = new Stack();
@@ -30,6 +46,10 @@ console.log(stack._storage)
 stack.push("general kenobi");
 console.log(stack._length)
 console.log(stack._storage)
-stack.pop();
+console.log(stack.peek());
+console.log(stack.pop());
 console.log(stack._length)
 console.log(stack._storage)
+
+//edge cases
+//what if user pushes empty value?
